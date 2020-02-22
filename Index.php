8a12ecca697a4752d6c453d29322a839db79c44b
jcolorado0419 <jcolorado0419@gmail.com>
2020-02-21 21:04:08 -0500

    Primer Ejercicio

--- Index.php
+++ Index.php
@@ -1,4 +1,18 @@
 <?php
    
-   echo("Hola Mundo");
+   echo("Ejercicio Elaborado por Julián Andrés Colorado Castañeda y Yuliana Andrea Paniagua <br>");
+   echo"<br> Ejercicio 1";
+
+   //Ejercicio 1
+   $numero1=10; 
+   $numero2=50;
+
+   $suma=$numero1+$numero2;
+   $resta=$numero1-$numero2;
+   $multiplicacion=$numero1*$numero2;
+   $division=$numero1/$numero2;
+
+   echo("<br>La suma de:".$numero1."+".$numero2."=".$suma);
+   
+
 ?>
